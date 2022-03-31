7e4a07271d73b840b3c787cb82e5694595e7ea6a
Ankur Seth <iankurseth@gmail.com>
2022-03-31 10:15:10 +0530

    Deleted copy and assignment constructors

--- include/Baseclass.hpp
+++ include/Baseclass.hpp
@@ -1,9 +1,24 @@
 #ifndef V20_BASECLASS_HPP
 #define V20_BASECLASS_HPP
 
+#include <iostream>
+
 namespace v20 {
 	class Baseclass {
-		//
+		Baseclass (const Baseclass&) = delete;
+		Baseclass& operator= (const Baseclass&) = delete;
+
+	public:
+		Baseclass () {}
+
+	protected:
+		void logConstruction () {
+			std::cout << "Created Baseclass {}\n";
+		}
+
+		void logDestruction () {
+			std::cout << "Deleted Baseclass {}\n";
+		}
 	};
 }
 
